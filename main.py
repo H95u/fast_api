001b1bec6f33d94043e9030aebf7da546ed26d7c
H95u <hieu.dh3t1@gmail.com>
2023-10-30 15:51:59 +0700

    update cors

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
+import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from controller import user_controller, checking_img_controller
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins
+    allow_methods=["*"],  # Allow all HTTP methods
+    allow_headers=["*"],  # Allow all headers
+    allow_credentials=False,  # Disable credentials (cookies, auth)
+    expose_headers=["*"],  # Expose all headers
+)
+
 app.include_router(user_controller.router)
 app.include_router(checking_img_controller.router)
+
 
