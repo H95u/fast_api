26ca6e3404f8bd3b75cdc5c08174f450e901f9ee
H95u <hieu.dh3t1@gmail.com>
2023-11-03 14:47:50 +0700

    new fix

--- model/user_model.py
+++ model/user_model.py
@@ -47,62 +47,58 @@ class UserModel:
             return result
         return {"message": "No data found"}
 
+    def add_user_model(self, data: User):
+        sql = "INSERT INTO user (link_avatar, user_name, ip_register, device_register, password, email, count_sukien, count_comment, count_view) " \
+              "VALUES (%(link_avatar)s, %(user_name)s, %(ip_register)s, %(device_register)s, %(password)s, %(email)s, 0, 0, 0)"
 
-def add_user_model(self, data: User):
-    sql = "INSERT INTO user (link_avatar, user_name, ip_register, device_register, password, email, count_sukien, count_comment, count_view) " \
-          "VALUES (%(link_avatar)s, %(user_name)s, %(ip_register)s, %(device_register)s, %(password)s, %(email)s, 0, 0, 0)"
+        try:
+            self.cur.execute(sql, data.dict())  # Use data.dict() to convert the Pydantic model to a dictionary
+            if self.cur.rowcount > 0:
+                return JSONResponse(content={"message": "CREATED_SUCCESSFULLY"}, status_code=201)
+            else:
+                return JSONResponse(content={"message": "Failed to create user"}, status_code=500)
+        except Exception as e:
+            # Handle database errors and return an appropriate response
+            return JSONResponse(content={"message": "Database error"}, status_code=500)
 
-    try:
-        self.cur.execute(sql, data.dict())  # Use data.dict() to convert the Pydantic model to a dictionary
+    def patch_user_model(self, data, uid):
+        qry = "UPDATE user SET "
+        for key in data:
+            if key != 'id_user':
+                qry += f"{key}='{data[key]}',"
+        qry = qry[:-1] + f" WHERE id_user = {uid}"
+
+        self.cur.execute(qry)
         if self.cur.rowcount > 0:
-            return JSONResponse(content={"message": "CREATED_SUCCESSFULLY"}, status_code=201)
+            return JSONResponse({"message": "UPDATED_SUCCESSFULLY"}, 201)
         else:
-            return JSONResponse(content={"message": "Failed to create user"}, status_code=500)
-    except Exception as e:
-        # Handle database errors and return an appropriate response
-        return JSONResponse(content={"message": "Database error"}, status_code=500)
+            return JSONResponse({"message": "NOTHING_TO_UPDATE"}, 204)
 
+    def delete_user_model(self, uid: int):
+        self.cur.execute("DELETE FROM user WHERE id_user = %(uid)s", {"uid": uid})
+        if self.cur.rowcount > 0:
+            return {"message": "DELETED_SUCCESSFULLY"}
+        return {"message": "CONTACT_DEVELOPER"}
 
-def patch_user_model(self, data, uid):
-    qry = "UPDATE user SET "
-    for key in data:
-        if key != 'id_user':
-            qry += f"{key}='{data[key]}',"
-    qry = qry[:-1] + f" WHERE id_user = {uid}"
+    def update_user_model(self, data, uid: int):
+        sql = (
+            "UPDATE user SET link_avatar=%s, user_name=%s, ip_register=%s, "
+            "device_register=%s, password=%s, email=%s WHERE id_user=%s"
+        )
 
-    self.cur.execute(qry)
-    if self.cur.rowcount > 0:
-        return JSONResponse({"message": "UPDATED_SUCCESSFULLY"}, 201)
-    else:
-        return JSONResponse({"message": "NOTHING_TO_UPDATE"}, 204)
+        values = (
+            data.get('link_avatar', None),
+            data.get('user_name', None),
+            data.get('ip_register', None),
+            data.get('device_register', None),
+            data.get('password', None),
+            data.get('email', None),
+            uid
+        )
 
+        self.cur.execute(sql, values)
 
-def delete_user_model(self, uid: int):
-    self.cur.execute("DELETE FROM user WHERE id_user = %(uid)s", {"uid": uid})
-    if self.cur.rowcount > 0:
-        return {"message": "DELETED_SUCCESSFULLY"}
-    return {"message": "CONTACT_DEVELOPER"}
-
-
-def update_user_model(self, data, uid: int):
-    sql = (
-        "UPDATE user SET link_avatar=%s, user_name=%s, ip_register=%s, "
-        "device_register=%s, password=%s, email=%s WHERE id_user=%s"
-    )
-
-    values = (
-        data.get('link_avatar', None),
-        data.get('user_name', None),
-        data.get('ip_register', None),
-        data.get('device_register', None),
-        data.get('password', None),
-        data.get('email', None),
-        uid
-    )
-
-    self.cur.execute(sql, values)
-
-    if self.cur.rowcount > 0:
-        return JSONResponse({"message": "UPDATED_SUCCESSFULLY"}, 201)
-    else:
-        return JSONResponse({"message": "NOTHING_TO_UPDATE"}, 204)
+        if self.cur.rowcount > 0:
+            return JSONResponse({"message": "UPDATED_SUCCESSFULLY"}, 201)
+        else:
+            return JSONResponse({"message": "NOTHING_TO_UPDATE"}, 204)
